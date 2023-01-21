b143e1128c8925605f1a9753b77dfa1270123460
cshjsc <juan@juans.dev>
2023-01-21 21:55:42 +0100

    updated doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
+//! Simple library to easily communicate with a tp link smart device.
+//! # Usage
+//! ```rust
+//! use std::io;
+//! use std::net::TcpStream;
+//!
+//! use tplink_shome_protocol::{receive_message, send_message};
+//!
+//! fn main() -> io::Result<()> {
+//!     let stream = TcpStream::connect("192.168.1.1:9999")?;
+//!     let raw = r#"{"system":{"get_sysinfo":{}}}"#;
+//!     send_message(&stream, raw)?;
+//!     let message = receive_message(&stream)?;
+//!     ...
+//! }
+//! ```
 use std::{
     io::{self, Read, Write},
     net::TcpStream,
